41c896aa6e31e51de1538b55e26c0ca7b4ee8c16
sunshine <thesunshineofficial@gmail.com>
2022-02-13 14:20:26 +0300

    Burger menu added

--- js/script.js
+++ js/script.js
@@ -13,6 +13,12 @@ if (menuLinks.length > 0) {
             const gotoBlock = document.querySelector(menuLink.dataset.goto);
             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('.header-container').offsetHeight;
 
+            if (iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
             window.scrollTo({
                 top: gotoBlockValue,
                 behavior: 'smooth'
@@ -46,4 +52,16 @@ function changeTheme(e) {
     }
 
     link.setAttribute('href', currTheme);
+}
+
+// Меню бургер
+
+const iconMenu = document.querySelector('.menu-icon');
+const menuBody = document.querySelector('.menu-body');
+if (iconMenu) {
+    iconMenu.addEventListener('click', function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
 }
